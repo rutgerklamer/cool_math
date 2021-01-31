aeda13c62eccfb0a4cc3639607c8ae49afdb42bc
= <rutger.klamer@gmail.com>
2021-01-31 11:08:33 +0100

    Added variable options

--- golden_ratio/main.js
+++ golden_ratio/main.js
@@ -4,7 +4,7 @@ let radius = width/2;
 let centerX = width/2;
 let centerY = height/2;
 let lineWeight = 1;
-let ratio, animate;
+let ratio, animate, prevValue;
 
 function setup() {
   createCanvas(width, height).position(screen.width/4, 0, 'absolute');
@@ -12,11 +12,8 @@ function setup() {
   strokeWeight(1)
   stroke(0,0,0);
   fill(254,255,12);
-  ellipse(width/2, height/2, radius*2, radius*2);
-  animate = true;
-  ratio = 1.61803398875;
-  draw();
   animate = false;
+  displaySeeds();
   ratio = 1.61803398875;
 }
 
@@ -29,18 +26,27 @@ function animates() {
   animate = !animate;
 }
 
+function displaySeeds() {
+  let flower = 0;
+  let angle = 0;
+    for (i = 0; i < document.getElementById("seeds").value; i++) {
+      flower += 0.3;
+      angle += 360 * (ratio);
+      let curX = (width/2) + flower * cos(angle * (Math.PI/180))
+      let curY = (width/2) + flower * sin(angle * (Math.PI/180))
+      ellipse(curX, curY, 25, 25);
+    }
+}
+
 function draw() {
+
   if (animate) {
     clear();
     ratio += deltaTime/10000000;
-    let flower = 0;
-    let angle = 0;
-      for (i = 0; i < 1200; i++) {
-        flower += 0.3;
-        angle += 360 * (ratio);
-        let curX = (width/2) + flower * cos(angle * (Math.PI/180))
-        let curY = (width/2) + flower * sin(angle * (Math.PI/180))
-        ellipse(curX, curY, 25, 25);
-      }
+    displaySeeds();
+  } else if (document.getElementById("seeds").value != prevValue) {
+    clear();
+    displaySeeds();
   }
+  prevValue = document.getElementById("seeds").value;
 }
